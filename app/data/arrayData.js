f78840356b832a34e9fc499a187d24010b655e70
shregupta89 <shregupta89@gmail.com>
2026-04-24 19:48:17 +0530

    Update Instagram social link to reels URL

--- app/data/arrayData.js
+++ app/data/arrayData.js
@@ -3,7 +3,7 @@ export const socialMediaIcons = [
     { id: 1, icon: <FaTwitter className="w-6 h-6 cursor-pointer" />, link: 'https://twitter.com/shregupta89' },
     { id: 2, icon: <FaGithub className="w-6 h-6 cursor-pointer" />, link: 'https://github.com/shregupta89' },
     { id: 3, icon: <FaLinkedin className="w-6 h-6 cursor-pointer" />, link: 'https://www.linkedin.com/in/shreya-gupta-0b6821255/' },
-    { id: 4, icon: <FaInstagram className="w-6 h-6 cursor-pointer" />, link: 'https://www.instagram.com/_shreyaguptaa_/' },
+  { id: 4, icon: <FaInstagram className="w-6 h-6 cursor-pointer" />, link: 'https://www.instagram.com/itsbyshreya/reels/' },
   ];
 
 export const skillsData = [
